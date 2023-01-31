a320b642130794d21727a371818833ce984a77dc
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-31 05:48:43 +0000

    [Finish] bookers1-debug

--- bookers1-debug/db/schema.rb
+++ bookers1-debug/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_30_133119) do
+ActiveRecord::Schema.define(version: 2023_01_31_051856) do
 
   create_table "books", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.text "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
